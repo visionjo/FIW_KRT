46345f2e68821ab1116554b181b7a6e5eec54266
Joseph Robinson <robinson.jo@husky.neu.edu>
2017-10-30 12:34:35 -0400

    Functions to load sets of features for list of pairs.

--- database/fiw.py
+++ database/fiw.py
@@ -25,6 +25,38 @@ logger.debug('Parse FIW')
 do_debug = False
 
 
+def read_feature(f_feature):
+    return np.genfromtxt(f_feature, delimiter=',')
+
+
+def load_all_features(dir_features, f_features):
+    """
+    Loads set of features.
+    :param dir_features: root directory containing features.
+    :param f_features: file paths of features relative to dir_features
+    :return: set of features
+    :rtype: dict
+    """
+    feats = []
+    for file in f_features:
+        feats.append(read_feature(dir_features + file))
+
+    return np.array(feats)
+
+
+def read_pair_list(f_csv):
+    pair_list = pd.read_csv(f_csv,delimiter=',', header=None)
+
+    folds = np.array(pair_list[0])
+    labels = np.array(pair_list[1])
+    pairs1 = pair_list[2].values
+    pairs1 = [p.replace(".jpg", ".csv").strip() for p in pairs1]
+    pairs2 = pair_list[3].values
+    pairs2 = [p.replace(".jpg", ".csv").strip() for p in pairs2]
+    return folds, labels, pairs1, pairs2
+
+
+
 def write_list_tri_pairs(fout, l_tuples):
     with open(fout, 'w') as out:
         csv_out = csv.writer(out)
@@ -573,8 +605,8 @@ if __name__ == '__main__':
     logger.info("Output Bin: {}\nFID folder: {}\n Anns folder: {}".format(out_bin, dir_fids, dir_fid))
 
     do_sibs = False
-    do_save = False
-    parse_fids = False
+    do_save = True
+    parse_fids = True
     logger.info("Parsing siblings: {}\nSaving Pairs: {}\n Parse FIDs: {}".format(do_sibs, do_save, parse_fids))
     if parse_fids:
         df_fam = prepare_fids(dir_fid=dir_fid, dirs_out=dir_fids)
