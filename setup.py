5cbfc767bc8e32143baccc88ced0461e0093796d
jRob <robinson.jo@husky.neu.edu>
2018-12-03 20:43:11 -0500

    update setup

--- setup.py
+++ setup.py
@@ -1,23 +1,29 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
+import setuptools
 
-
-with open('README.md') as f:
+with open('README.md', 'r') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open('LICENSE', 'r') as f:
     license = f.read()
 
 setup(
-    name='FIW_KRT',
-    version='1.0.0',
+    name='fiwtools',
+    version='0.1.0',
     description='Families In the WIld: A Kinship Recogntion Toolbox.',
     long_description=readme,
     author='Joseph Robinson',
     author_email='robinson.jo@husky.neu.edu',
-    url='https://github.com/huskyjo/FIW_KRT',
+    url='https://github.com/visionjo/FIW_KRT',
+    packages=setuptools.find_packages(),
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    # packages=find_packages(exclude=('tests', 'docs'))
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
 
