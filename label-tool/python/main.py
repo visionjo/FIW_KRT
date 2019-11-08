3f971b0fbe798847674896ed3d5879fc4d9e48dd
Joseph Robinson <robinson.jo@husky.neu.edu>
2019-11-08 16:12:18 -0500

    added step 2 to loop; write label proposals to CSV

--- label-tool/python/main.py
+++ label-tool/python/main.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 import os
 import tqdm
+from sklearn.svm import SVC
 
 def load_features(din, key='n*'):
     """
@@ -24,7 +25,10 @@ def load_features(din, key='n*'):
     return {f.split('/')[-1].replace('.pkl', ''): pd.read_pickle(f) for f in files_feats_outer}, \
            {f.split('/')[-1].replace('.pkl', ''): pd.read_pickle(f) for f in files_feats_inner}
 
+param_grid = {'C': [1e3, 5e3, 1e4, 5e4, 1e5],
+              'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1], }
 
+threshold_sim_score = 0.32
 # params
 dir_features = '/home/jrobby/master-version/fiwdb/fiw_plus_vgg-cropped-features2/'
 dir_faces = '/home/jrobby/master-version/fiwdb/fiw_plus_vgg-cropped2/'
@@ -67,10 +71,29 @@ for i, dir_in in enumerate(tqdm.tqdm(dirs_in)):
         # save all features
         pd.to_pickle(feats, dir_out + mids[i].replace('/', '_') + '.pkl')
 
+        # determine which faces being added are true matches
+        mean_scores = scores.mean(axis=0)
+
+        mask = (threshold_sim_score < mean_scores).astype(np.uint8)
+
+        df = pd.DataFrame(np.array([list(feats_new.keys()), mask, mean_scores]).T,
+                          columns=['file', 'label', 'score'])
+
+        df.to_csv( dir_out + mids[i].replace('/', '_') + '-labels' + '.csv', index=False)
+
 ##
 #  PART 2
 ##
 feature_files = glob.glob(dir_out + '*.pkl')
 
 print("{} subjects assumed to be the same".format(len(feature_files)))
+
 # save results in format compatible with GUI
+
+# for each subject, filter out non-matched faces
+
+# provide labels: 1 if true match; else 0
+
+# write CSV to disc
+
+# prepare to pass to JAVA tool
